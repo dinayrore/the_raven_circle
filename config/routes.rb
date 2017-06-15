4c435c625937506b50d3fb7a5122833a163520e9
Kristine <kteich88@gmail.com>
2017-06-15 10:51:20 -0400

    Updated routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,24 @@
 Rails.application.routes.draw do
+  root to: 'pages#home'
+
+  get  '/hark',     to: 'pages#hark'
+  get  '/hark/:id', to: 'posts#show'
+  get  '/raven',    to: 'pages#about'
+  get  '/contact',  to: 'pages#contact'
+
+  get 'password_resets/new'
+  get 'password_resets/edit'
+
+  get    '/signup',  to: 'users#new'
+  post   '/signup',  to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
+  resources :users
+  resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
+  resources :posts do
+  	resources :comments
+  end
 end
