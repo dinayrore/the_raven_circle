88736ea78bc0e8356c068260c8057cde4e74b3df
Kristine <kteich88@gmail.com>
2017-06-16 13:02:24 -0400

    Created seeds file for older blog posts.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,86 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Post.create([
+  { title: 'Understanding Love',
+    body: "Saying  I love you has become such a common expression, most don't see it for what it is. Most know of falling in love or mending a broken heart, but love is more than a fleeting moment of emotion but a conscious choice, so much so that Buddhist who pride themselves on a mindful way of love, have there own heart mantra to follow and practice . What is it to love? Some feel loving others is an easy task, "Love thy neighbor", once you have the practice of loving, it becomes a life style and is seen as easy. But until you have that moment to understand love, loving others and yourself can be a challenge.
+
+    What is it to love?
+    each person's feeling on what love is and how they express it may be different. One truth is universal, that is acceptance. Taking it in, as what it is not what you wish it to be and valuing it for exactly as it is that day. It may do something that bothers you, or create a problem, which will happen. When you love those items become a separate thing and not attached to what you love. It be like loving stake for what it is, and going to a restaurant that doesn't cook it correctly. You should still love the stake, and if done correctly you will even have stake again at that restaurant. for within love there is forgiveness, understanding and gratitude.
+
+    How does one show love?
+    How a person can express love just as different as identifying it. Normally how people express love is with the value system they are most comfortable with. If it be by giving gifts, by giving compliments, or by giving time.  Acts of love can go unnoticed, that can lead a person to frustrating feelings. Reminding yourself that the act of love isn't about feeling appreciated as it is to give appreciation. One of the biggest oversight when showing love, is to understand their value system. If it is natural for you to give gifts but you are dealing with a person that values times. They may view your acts not as you intend. you need to use the right love currency for the right time. This means to be successful you need to understand how the person, place or thing, views love and what is natural for expression on their terms.
+
+
+    How does one receive love?
+    There is a saying that you are the best version of you that can be, and you do that perfectly. By understanding the way you view love and others will help with receiving it. There is no task you need to preform no outfit to wear no goal to reach to be worthy of love. Before you can feel comfortable to receive love from others you need to first receive it from yourself.  Your house is only as strong as your foundation, same when it come to love. how can you fully love others if you cannot love yourself. for others tend to be a reflection of our own internal world.
+
+    The power of love
+    Love is a powerful thing, not just to create but also to witness. The act of love, witness or pursuer, has a magic of its own. It can change the whole energy of a moment, from mundane to extraordinary. Some of the most powerful images and actions came from a loving act. However this idea of the power of love, the strength of love should not be shocking. Most of us have heard of one tale or another talking about how love saved the day. If it was freeing those imprisoned or finding the diamond in the rough.
+
+    Love effects our lives differently but once embraced those powerful images will be internal, you will be saving yourself and others from the towers of false truth. It is not without its battles. Just because you chose to life a life to love does not mean all you show love too will. You will be tested maybe never recognized for your efforts. At times to love those around you is to be silent and be a punching bag. yet again not an easy task and for me at times challenging. Most parents will understand that when watching a child about to do something you know will end badly so they can learn for themselves and problem solve. I find the most valuable things in life are always a process to discover, because without working for something you value if it be understanding you may just miss the beautiful value that it can have on your life.
+
+    So know That to be me my circle I will and always have loved you. #deepwaters",
+    user: User.first,
+    created_at: 06-07-2017
+  },
+
+  { title: 'Total Body Combat',
+    body: "THE CREATURE LIVES
+
+    For some miracle graced by the gods I am awake, In the morning hours before my job. Lets just say I am a my-bed-is-disney-world kinda gal. So leaving it in the cold icy chill of the morning, knowing I could stay snuggled up with my BFF’s blanket and pillow, is easily my first challenge. But I did leave. I am trying this body combat class as my first one it sounds fun. Reminded me of the Tibo workouts I did in middle school at gym because that's probably the last time I had worked out.
+
+    I explained it's been awhile since I’ve been with a Jim, i mean gym
+
+    So I run about 15 minutes late to the 5: 45 am class. If I didn’t have to swipe and them look at me to see I was there, no one would believe I was up, dressed and had risen. I find my way to the work out in progress, and they start this set where you punch imaginary monster, squat and body slam the floor into a plank position to jump back up to punch the nothingness and so forth and so forth.
+
+    Ok so I got maybe two in until I hear the gods say, We have help you arise this morning, you are on your own for this. I was like really? The response that was given was even Aphrodite has no love for you. So the peppy and sweet girl tells us you can modify the set. As I have a moment between gasping for air and actually seeing out of the pool of sweat that has become my body. I think most will be modifying this.
+
+    BUT MY ONE BELIEF IS TRUE...
+
+    The people who get up at this time of day are bionic bunnies human clones of some sort. Only us 3 normal Earthian’s are doing the human version. I had no idea what is in their water; if they were ever human to begin with but the urge to call the x files were overwhelming.
+
+    THEN I LEARNED HOW IT GOT ITS NAME TOTAL BODY COMBAT
+
+
+    After the set I had to leave, and then the combat began… Over to the trashcan I found myself. Watching the sweet treat of last night also be arisen. Between the moments of holding myself over the can I thought that this was I guess more of my ab exercises of the day.
+    I lasted maybe 20 min in that hour class, I did come back to find out I came in at the hardest time in that class and to try again.
+
+    THEY CALL THOSE SETS “BURPBEE’S”
+
+    Do not be deceived by the name, that cute adorable sounding thing could quite possibly be the thing that kills me. It's like calling a bomb a biggy-wiggy boom boom. I feel they need to change this name to death to all you knew before… Armageddon. And I shouldn’t be surprised with leaving my bed hours before work why wouldn’t this work out be the end of days?
+
+    SO I LIVE ANOTHER DAY
+    Someone may look at this day as a fail; a loss. But I don’t. Even with the body combat taking a turn more internal combat then external combat it was a huge win. For it was the end of days, the end of days that i have said this wasn’t important to me. So you may wonder why I look at this as a win, well...
+
+    I CAME
+    I decided this was important, got myself together and came.
+
+    I TRIED
+    I could of just said nope, but I didn’t I still got through a set and a half
+
+
+                                         And Most Importantly........
+                             !!!!!!!!!!!!   I AM NOT IMPORTANT   !!!!!!!!!!!!!
+
+    No one cares about me, my existence has no value, I am empty space
+
+
+    The last one may not seem like a victory, but It was the best one I had. I am not important, no one cares about me, I didn’t matter! Being the plus size girl needing to wear skin tight clothes I wouldn’t chose to normally, get beat red sweating ,and gasping for air. You don’t feel amazing or comfortable. I have in the past felt judged, vulnerable,even ashamed. But today, I didn’t matter to anyone. Could be they were focused on their air supply too, but it was a powerful moment. I did not need to carry the weight of those eyes in my workout. The people in the advance state did not rock my world in what I was doing. If nothing else when I saw that teacher I felt respected. She understood and valued that I decided this was important and did the best I could at the place I am at right now. My body may feel like it's dying but I am here and I am not ready to let some discomfort rock my boat on that life I deserve. To be healthy mind body and spirit! #momentinmotion",
+    user: User.first,
+    created_at: 06-07-2017
+  },
+  { title: 'What is a Name',
+    body: "
+    What is a name but a place holder of the ego, a label on a container. What is the purpose of a name? to call this space to the left Henry then to say this is space. What is a name labeling? Is this name to put claim on this vessel? The skin and bones, the meat of the matter of the star dust reformed. Is the name on the energy within, trying to say although collective this is different because this energy, this spirit is in this box, we call Sally? Dose a name share both this tether from body to above this connection called Mary. Would not these connections be the same, are we not all made of meat, have we all had an ego? So why a name? If we are everything and nothing, if we are energy flowing and solid in form, if we are a walking contraction and completion. A name would be suiting wouldn't it.
+
+    So why so much thought so much power be placed into something most wont take the time to look at, to discover the significance for themselves. What luck would the name Zeus give a child? what insight named after a prophet? what personality given a color or an object. Yet we spend hours searching for this name for a child or within ourselves. I always felt like a Becky, deep down I am a Johnny. Names to reward and to punish.
+
+    What are all objects in the mind; in the hand but tools. Could a name be just that, a tool to deal with life. A way to communicate with those who lives in their own world, only a name can travel between untarnished. For what they see in a name, they chose. Is it the truth of our universal language, that books may be called something different. the world for some is round or flat. A name or a clusters of sounds are what we all, where ever in the the world. know to identify the I; the I am. A lesson taught as old as time, throughout the records of man. But never seaming to answer the question worth asking...
+
+    What am I",
+    user: User.first,
+    created_at: 04-20-2017
+
+  }
+  ])
